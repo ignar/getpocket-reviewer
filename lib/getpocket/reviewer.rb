34db4467528064ed5861b931a571443188bec814
Artem Melnykov <artem.melnykov@shopify.com>
2020-03-12 17:23:56 -0400

    Initial commit

---
+++ lib/getpocket/reviewer.rb
@@ -0,0 +1,9 @@
+require "getpocket/reviewer/version"
+require 'faraday'
+require 'json'
+
+module Getpocket
+  module Reviewer
+    class Error < StandardError; end
+  end
+end
---
+++ lib/getpocket/reviewer/version.rb
@@ -0,0 +1,5 @@
+module Getpocket
+  module Reviewer
+    VERSION = "0.1.0"
+  end
+end
