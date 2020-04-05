77f29bb36960937665ec8d68cc548568aa77c7e4
Artem Melnykov <artem.melnykov@shopify.com>
2020-04-04 20:52:49 -0400

    Fix request token specs

--- spec/getpocket/request_token_spec.rb
+++ spec/getpocket/request_token_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-RSpec.describe(Getpocket::RequestToken) do
+RSpec.describe(Getpocket::Reviewer::RequestToken) do
   subject { described_class.new(consumer_key: 'consumer-key', redirect_url: 'redirect-url') }
 
   describe '#consumer_key' do
@@ -12,15 +12,15 @@ RSpec.describe(Getpocket::RequestToken) do
     it { expect(subject).to(respond_to(:request_token)) }
 
     it 'has a default value' do
-      expect(subject.request_token).to eq('')
+      expect(subject.request_token).to(be_empty)
     end
 
     context 'when value given on initialization' do
-      subject { described_class.new(consumer_key: 'consumer-key', redirect_url: 'redirect-url', request_token: 'token')  }
+      subject { described_class.new(consumer_key: 'consumer-key', redirect_url: 'redirect-url', request_token: 'token') }
 
       it 'stores given value' do
-        described_class.new(consumer_key: 'consumer-key', redirect_url: 'redirect-url') 
-      end 
+        described_class.new(consumer_key: 'consumer-key', redirect_url: 'redirect-url')
+      end
     end
   end
 
