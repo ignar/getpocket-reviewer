723cbce40d4f2058271c9d8826e566dbf6d6b21a
Artem Melnykov <artem.melnykov@shopify.com>
2020-04-11 08:29:42 -0400

    Repository with fetch action

--- lib/getpocket/repository.rb
+++ lib/getpocket/repository.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 module Getpocket
   class Repository
+    attr_reader :config
+
+    def initialize(config)
+      @config = config
+    end
+
     def add; end
 
     def modify; end
 
-    def retrieve(since: nil)
-      fetcher = Operations::Fetcher.new
-      fetcher.call(since: since)
+    def retrieve(since: nil, per_page:)
+      fetcher = Operations::Fetcher.new(config)
+      fetcher.call(since: since, per_page: per_page)
     end
   end
 end
--- spec/getpocket/repository_spec.rb
+++ spec/getpocket/repository_spec.rb
@@ -2,29 +2,44 @@
 require 'spec_helper'
 
 RSpec.describe Getpocket::Repository do
+  let(:config) { double }
+
   describe '#add'
   describe '#modify'
 
   describe '#retrieve' do
     let(:fetcher_stub) { double }
+
     before do
       allow(Getpocket::Operations::Fetcher).to receive(:new).and_return(fetcher_stub)
     end
 
+    shared_examples 'maps entities' do
+      it 'returns a collection' do
+        expect(result).to be_kind_of(Array)
+      end
+
+      it 'returns mapped entities' do
+        expect(result.first).to be_kind_of(Getpocket::Article)
+      end
+    end
+
     context 'when no page given' do
-      subject(:result) { described_class.new.retrieve }
+      subject(:result) { described_class.new(config).retrieve(per_page: 1) }
+
+      include_examples 'maps entities'
 
       it 'uses retriever operation' do
-        expect(fetcher_stub).to receive(:call).with(since: nil).and_return([])
+        expect(fetcher_stub).to receive(:call).with(since: nil, per_page: 1)
         result
       end
     end
 
     context 'when page number is given' do
-      subject(:result) { described_class.new.retrieve(since: Time.now) }
+      subject(:result) { described_class.new(config).retrieve(since: Time.now, per_page: 1) }
 
       it 'uses retriever operation' do
-        expect(fetcher_stub).to receive(:call).with(since: kind_of(Time)).and_return([])
+        expect(fetcher_stub).to receive(:call).with(since: kind_of(Time), per_page: 1).and_return([])
         result
       end
     end
